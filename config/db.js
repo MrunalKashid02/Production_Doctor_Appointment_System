c22fe9fc305a21061341628ab29f7f86f5d80b52
Mrunal Kashid <95683323+MrunalKashid02@users.noreply.github.com>
2023-11-21 19:11:14 +0530

    some changes

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const colors= require('colors')
 
 const connectDB = async() =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(env.MONGODB_URL)
         console.log(`MongoDB connected to ${mongoose.connection.host}`.bgGreen.white)
     } catch (error) {
         console.log(`MongoDB Server Issue ${error}`.bgRed.white);
